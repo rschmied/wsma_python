9d0672521f75d3ef0322bc7b299db37605951096
Ralph Schmieder <rschmied@cisco.com>
2016-11-10 13:39:16 +0100

    made examples work w/ Python3
    - added print_function import
    - made 00show_run use wsma_exec_text()
    
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- examples/00show_run.py
+++ examples/00show_run.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from wsma.WSMA import WSMA
 from wsma_config import WSMA_IP, WSMA_USER, WSMA_PASSWORD
 import json
 
-wsma=WSMA(WSMA_IP, WSMA_USER, WSMA_PASSWORD)
-result = wsma.wsma_exec("show run")
-print result['response']['execLog']['dialogueLog']['received']['text']
+wsma = WSMA(WSMA_IP, WSMA_USER, WSMA_PASSWORD)
+result = wsma.wsma_exec_text("show run")
+print(result)
--- examples/04_config_rollback.py
+++ examples/04_config_rollback.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from wsma.WSMA import WSMA
 from wsma_config import WSMA_IP, WSMA_USER, WSMA_PASSWORD
 import json
 import logging
 
 # comment this out to turn off debug
-#logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
+wsma = WSMA(WSMA_IP, WSMA_USER, WSMA_PASSWORD)
 
-wsma=WSMA(WSMA_IP, WSMA_USER, WSMA_PASSWORD)
-
-result = wsma.wsma_config("ip access-list extended 101\n permit ip any any \nfed", action_on_fail='rollback')
-print json.dumps(result, indent=4)
+result = wsma.wsma_config("ip access-list extended 101\n permit ip any any \nend", action_on_fail='rollback')
+print(json.dumps(result, indent=4))
