0020c79dcf28216fd38a845810bc8b7d4c9fe91d
Ralph Schmieder <rschmied@cisco.com>
2016-11-14 13:43:30 +0100

    bumped version
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
-
 from setuptools import setup, find_packages
 setup(
     name = "wsma",
-    version = "0.3.1",
+    version = "0.4.1",
     packages = find_packages(),
     install_requires = ["requests", "jinja2", 'xmltodict', 'paramiko']
-
 )
