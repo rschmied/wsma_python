de9011c22559cb2d22953bedfd25f15a4b2b14b0
Ralph Schmieder <ralph.schmieder@gmail.com>
2016-11-18 00:32:53 +0100

    bumped revision

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "wsma",
-    version = "0.4.1",
+    version = "0.4.2",
     packages = find_packages(),
     install_requires = ["requests", "jinja2", 'xmltodict', 'paramiko']
 )
